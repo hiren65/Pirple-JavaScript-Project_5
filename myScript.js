2c1962d0bb186b417be506d0a98045fb1a331b68
utkarsh patel <hirenkumar@me.com>
2019-05-28 20:32:20 +1000

    Chart,js attached in html and instance created in myscript.js

---
+++ myScript.js
@@ -0,0 +1,42 @@
+//alert();
+let myType;
+let createChart;
+
+let selected = document.getElementById("select");
+
+
+selected.addEventListener("click",function () {
+   //alert();
+
+    let myV = selected.options[selected.selectedIndex].value;
+    var grapharea = document.getElementById("myChart").getContext("2d");
+    if (createChart){
+        createChart.destroy();
+    }
+ createChart =   new Chart(grapharea, {
+        type: myV,
+        data: {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [
+                {
+                    label: "Population (millions)",
+                    backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                    data: [2478,5267,734,784,100]
+                }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
+            }
+        }
+
+    });
+
+
+
+});
+
+
+
