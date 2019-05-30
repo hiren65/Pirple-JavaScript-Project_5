48aea5dd6bd0dd98c8a9f996731cc54ff11c8a15
utkarsh patel <hirenkumar@me.com>
2019-05-30 23:12:43 +1000

    chart config and data edit inout box added

--- myScript.js
+++ myScript.js
@@ -3,6 +3,15 @@ let myType;
 let createChart;
 let title = 'Predicted world population (millions) in 2050';
 
+let arrA = ["Africa", "Asia", "Europe", "Latin America", "North America"];
+let arrColor = ["rgba(255,99,132,0.6)",
+    "rgba(54,162,232,0.6)",
+    "rgba(255,206,66,0.6)",
+    "rgba(75,192,192,0.6)",
+    "rgba(55,99,32,0.6)"];
+let arrData = [2478,5267,734,784,100];
+
+let strArr = [];
 
 let selected = document.getElementById("select");
 window.onload = function(){
@@ -28,16 +37,12 @@ function drawMe() {
         createChart =   new Chart(grapharea, {
             type: myV,
             data: {
-                labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+                labels: arrA,
                 datasets: [
                     {
                         label: "Population (millions)",
-                        backgroundColor: ["rgba(255,99,132,0.6)",
-                                          "rgba(54,162,232,0.6)",
-                                          "rgba(255,206,66,0.6)",
-                                          "rgba(75,192,192,0.6)",
-                                          "rgba(255,99,132,0.6)"],
-                        data: [2478,5267,734,784,100],
+                        backgroundColor: arrColor,
+                        data: arrData,
                         borderWidth:1,
                         borderColor:"silver",
                         hoverBorderColor:"blue",
@@ -76,10 +81,32 @@ function showConfig() {
                      <span>Title</span>
                      <input type="text" id="tlt1">
                      
+                     
                      <button id="btn01">Apply</button>
     `;
     let apply = document.getElementById("btn01");
     apply.addEventListener("click",pushChange);
+}
+
+let dataF = document.getElementById("dataFill");
+let dataApplyBtn = document.getElementById("dataApplyBtn");
+dataApplyBtn.addEventListener("click",createDataAdd);
+function createDataAdd() {
+    //alert();
+    let input11 = document.getElementById("input11").value;
+    arrA = input11.split(",");
+
+    let input22 = document.getElementById("input22").value;
+    arrColor = input22.split(",");
+
+    let realData = document.getElementById("realData").value;
+    arrData = realData.split(",");
+
+    //alert(newValue);
+
+
+    drawMe();
+    //alert(arr);
 }
 
 
@@ -91,4 +118,22 @@ function pushChange() {
         return;
     }
    drawMe();
+}
+let addColor = document.getElementById("addColor");
+addColor.addEventListener("click",addColorFromPicker);
+function addColorFromPicker() {
+    let myColor = document.getElementById("myColor");
+    let newValue = myColor.value;
+
+    strArr.push(newValue);
+    document.getElementById("input22").value = strArr;
+}
+
+let dataClear = document.getElementById("dataClearBtn");
+dataClear.addEventListener("click",clearAllData);
+function clearAllData(){
+    strArr = [];arrColor = [];arrData = [];
+    document.getElementById("realData").value = "";
+    document.getElementById("input22").value = "";
+    document.getElementById("input11").value = "";
 }
