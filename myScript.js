363f8693988faf2d8dc9034a0674dff854f99a49
utkarsh patel <hirenkumar@me.com>
2019-06-02 22:03:13 +1000

    import csv file bug fixes and more

--- myScript.js
+++ myScript.js
@@ -350,7 +350,7 @@ function addTransfarencyInRndomColorArr(arr) {
 }
 
 
-var fileInput = document.getElementById("csv");
+var fileInput = document.getElementById("csvFileInput");
 
     readFile = function () {
         var reader = new FileReader();
@@ -362,7 +362,7 @@ var fileInput = document.getElementById("csv");
 
     };
 
-fileInput.addEventListener('change', readFile);
+//fileInput.addEventListener('change', readFile);
 
 
 function handleFileSelect(evt) {
@@ -380,7 +380,7 @@ function handleFileSelect(evt) {
     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
 }
 
-document.getElementById('csv').addEventListener('change', handleFileSelect, false);
+//document.getElementById('csv').addEventListener('change', handleFileSelect, false);
 
 
 //check for file
@@ -432,3 +432,138 @@ window.onload = function() {
         }
     });
 };*/
+function handleFiles(files) {
+    // Check for the various File API support.
+    if (window.FileReader) {
+        // FileReader are supported.
+        getAsText(files[0]);
+        //loadHandler();
+
+    } else {
+        alert('FileReader are not supported in this browser.');
+    }
+}
+
+
+function getAsText(fileToRead) {
+    var reader = new FileReader();
+    // Handle errors load
+    reader.onload = loadHandler;
+    reader.onerror = errorHandler;
+    // Read file into memory as UTF-8
+    reader.readAsText(fileToRead);
+}
+
+function loadHandler(event) {
+    var csv = event.target.result;
+    processData(csv);
+}
+
+function processData(csv) {
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+    while (allTextLines.length) {
+        lines.push(allTextLines.shift().split(','));
+    }
+    console.log(lines);
+    console.log(lines[0]);
+    let pp =[];
+    let dd = [];
+    countryArr = [];
+    valueArr = [];
+    arrColor = [];
+    arrTempColor = [];
+    for (let i=1;i<lines.length;i++) {
+        pp.push(lines[i][0]);
+        dd.push(lines[i][1]);
+
+        countryArr.push(lines[i][0]);
+        valueArr.push(lines[i][1]);
+        //console.log(data[i].Value);
+        let c1 = getRandomColor();
+        arrTempColor.push(c1);
+
+
+        title = lines[1][2];
+        subTitleLabel = lines[1][3];
+        //alert(title);
+
+
+    }
+    arrColor = addTransfarencyInRndomColorArr(arrTempColor);
+    console.log("ffff     "+arrColor);
+    document.getElementById("input11").value = countryArr;
+    document.getElementById("realData").value = valueArr;
+    document.getElementById("input22").value = arrColor;
+    //countryArr = pp;valueArr = dd;
+    //drawMe();
+    //setTimeout(drawMe,3000);
+    //console.log(countryArr);console.log(valueArr);
+    //console.log(arrColor);
+    drawOutput(lines);
+}
+
+//if your csv file contains the column names as the first line
+function processDataAsObj(csv){
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+
+    //first line of csv
+    var keys = allTextLines.shift().split(',');
+
+    while (allTextLines.length) {
+        var arr = allTextLines.shift().split(',');
+        var obj = {};
+        for(var i = 0; i < keys.length; i++){
+            obj[keys[i]] = arr[i];
+        }
+        lines.push(obj);
+    }
+    console.log(lines);
+    drawOutputAsObj(lines);
+}
+
+function errorHandler(evt) {
+    if(evt.target.error.name == "NotReadableError") {
+        alert("Canno't read file !");
+    }
+}
+
+function drawOutput(lines){
+    //Clear previous data
+    document.getElementById("output").innerHTML = "";
+    var table = document.createElement("table");
+    for (var i = 0; i < lines.length; i++) {
+        var row = table.insertRow(-1);
+        for (var j = 0; j < lines[i].length; j++) {
+            var firstNameCell = row.insertCell(-1);
+            firstNameCell.appendChild(document.createTextNode(lines[i][j]));
+        }
+    }
+    document.getElementById("output").appendChild(table);
+}
+
+//draw the table, if first line contains heading
+function drawOutputAsObj(lines){
+    //Clear previous data
+    document.getElementById("output").innerHTML = "";
+    var table = document.createElement("table");
+
+    //for the table headings
+    var tableHeader = table.insertRow(-1);
+    Object.keys(lines[0]).forEach(function(key){
+        var el = document.createElement("TH");
+        el.innerHTML = key;
+        tableHeader.appendChild(el);
+    });
+
+    //the data
+    for (var i = 0; i < lines.length; i++) {
+        var row = table.insertRow(-1);
+        Object.keys(lines[0]).forEach(function(key){
+            var data = row.insertCell(-1);
+            data.appendChild(document.createTextNode(lines[i][key]));
+        });
+    }
+    document.getElementById("output").appendChild(table);
+}
